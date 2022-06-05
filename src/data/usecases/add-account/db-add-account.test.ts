672bbc8be8bca64e10e36bcef998024e0983da00
Eduardo Pereira <augusto.pereira@protonmail.com>
2022-06-04 23:33:05 -0300

    refactor: move DbAddAccount and encrypterStub creations to a factory helper method

--- src/data/usecases/add-account/db-add-account.test.ts
+++ src/data/usecases/add-account/db-add-account.test.ts
@@ -1,14 +1,32 @@
+import { Encrypter } from '../../interfaces/encrypter'
 import { DbAddAccount } from './db-add-account'
+
+interface makeSutProps {
+  sut: DbAddAccount
+  encrypterStub: Encrypter
+}
+
+const makeEncrypter = (): Encrypter => {
+  class EncrypterStub implements Encrypter {
+    async encrypt (value: string): Promise<string> {
+      return await new Promise(resolve => resolve('hashed_password'))
+    }
+  }
+  return new EncrypterStub()
+}
+
+const makeSut = (): makeSutProps => {
+  const encrypterStub = makeEncrypter()
+  const sut = new DbAddAccount(encrypterStub)
+  return {
+    sut,
+    encrypterStub
+  }
+}
 
 describe('DbAddAccount Usecase', () => {
   test('Should call encrypter with correct password', async () => {
-    class EncrypterStub {
-      async encrypt (value: string): Promise<string> {
-        return await new Promise(resolve => resolve('hashed_password'))
-      }
-    }
-    const encrypterStub = new EncrypterStub()
-    const sut = new DbAddAccount(encrypterStub)
+    const { sut, encrypterStub } = makeSut()
     const encryptSpy = jest.spyOn(encrypterStub, 'encrypt')
     const accountData = {
       name: 'valid_name',
